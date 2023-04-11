4f45ec1f1c78e16c1c3cb6716eb6d609bef2f1f7
tmcdonnell2 <tmcdonnell2@learn.athabascau.ca>
2023-04-10 17:45:37 -0700

    EmailService|Sender initialization + Inlcude ConfirmationTokenRepository|Service + supporting changes to Gifter repository and Registration controller and service

---
+++ src/main/java/com/giftr/registration/token/ConfirmationTokenRepository.java
@@ -0,0 +1,29 @@
+package com.giftr.registration.token;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface ConfirmationTokenRepository
+        extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query("""
+            UPDATE ConfirmationToken ct \
+            SET ct.confirmedAt = ?2 \
+            WHERE ct.token = ?1 \
+            """
+    )
+    int updateConfirmedAt(String token, LocalDateTime now);
+}
